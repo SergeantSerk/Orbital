a455d0e120f05df0699ad3cc53adec1b0427d60f
SergeantSerk <14278530+SergeantSerk@users.noreply.github.com>
2022-04-29 17:16:13 +0100

    Body colour metadata + breaking rendering

--- Orbital.Cli/Program.cs
+++ Orbital.Cli/Program.cs
@@ -16,61 +16,61 @@ namespace Orbital.Cli
             var bodies = new List<Body>
             {
                 new Body(
-                    metadata: new Metadata("Sun"),
+                    metadata: new Metadata("Sun", Color.Yellow),
                     position: Vector3.Zero,
                     velocity: Vector3.Zero,
                     mass: 1988500e24,
                     radius: 695700e3),
                 new Body(
-                    metadata: new Metadata("Mercury"),
+                    metadata: new Metadata("Mercury", Color.SlateGray),
                     position: new Vector3(57.909e9, 0, 0),
                     velocity: new Vector3(0, 47.36e3, 0),
                     mass: 0.33010e24,
                     radius: 2439.7e3),
                 new Body(
-                    metadata: new Metadata("Venus"),
+                    metadata: new Metadata("Venus", Color.LightYellow),
                     position: new Vector3(108.21e9, 0, 0),
                     velocity: new Vector3(0, 35.02e3, 0),
                     mass: 4.8673e24,
                     radius: 6051.8e3),
                 new Body(
-                    metadata: new Metadata("Earth"),
+                    metadata: new Metadata("Earth", Color.Cyan),
                     position: new Vector3(149.598e9, 0, 0),
                     velocity: new Vector3(0, 29.78e3, 0),
                     mass: 5.9722e24,
                     radius: 6371.0e3),
                 new Body(
-                    metadata: new Metadata("Mars"),
+                    metadata: new Metadata("Mars", Color.Red),
                     position: new Vector3(227.956e9, 0, 0),
                     velocity: new Vector3(0, 24.07e3, 0),
                     mass: 0.64169e24,
                     radius: 3389.5e3),
                 new Body(
-                    metadata: new Metadata("Jupiter"),
+                    metadata: new Metadata("Jupiter", Color.OrangeRed),
                     position: new Vector3(778.479e9, 0, 0),
                     velocity: new Vector3(0, 13.06e3, 0),
                     mass: 1898.13e24,
                     radius: 69911.0e3),
                 new Body(
-                    metadata: new Metadata("Saturn"),
+                    metadata: new Metadata("Saturn", Color.SandyBrown),
                     position: new Vector3(1432.041e9, 0, 0),
                     velocity: new Vector3(0, 9.68e3, 0),
                     mass: 568.32e24,
                     radius: 58232.0e3),
                 new Body(
-                    metadata: new Metadata("Uranus"),
+                    metadata: new Metadata("Uranus", Color.LightBlue),
                     position: new Vector3(2867.043e9, 0, 0),
                     velocity: new Vector3(0, 6.8e3, 0),
                     mass: 86.811e24,
                     radius: 25362.0e3),
                 new Body(
-                    metadata: new Metadata("Neptune"),
+                    metadata: new Metadata("Neptune", Color.Blue),
                     position: new Vector3(4514.953e9, 0, 0),
                     velocity: new Vector3(0, 5.43e3, 0),
                     mass: 102.409e24,
                     radius: 24622.0e3),
                 new Body(
-                    metadata: new Metadata("Pluto"),
+                    metadata: new Metadata("Pluto", Color.WhiteSmoke),
                     position: new Vector3(5869.656e9, 0, 0),
                     velocity: new Vector3(0, 4.67e3, 0),
                     mass: 0.01303e24,
@@ -78,16 +78,16 @@ namespace Orbital.Cli
             };
 
             // Maximum radius with padding
-            double zoom = 0.03;
+            double zoom = 1;
             double viewport = bodies
                 .Select(_ => _.Position.Magnitude)
                 .Max() * zoom;
-            // Add extra x% of space
+            // Add extra x% of padding space
             viewport += viewport * 0.01;
             var viewportVector = new Vector3(viewport);
 
-            double minEllipseRadius = 4f;
-            double ellipseRadius = bodies
+            double minEllipseRadius = 1f;
+            double minBodyRadius = bodies
                 .Select(_ => _.Radius)
                 .Min();
 
@@ -97,8 +97,6 @@ namespace Orbital.Cli
             double t_end = 86400 * 365 * 10; // approximately a decade in seconds
             double updateDt = 60*60*24; // simulation step (seconds)
             var dtVector = new Vector3(updateDt);
-
-            using var pen = new Pen(Color.White, 1f);
 
             string directoryName = "renders";
             string currentDirectory = Directory.GetCurrentDirectory();
@@ -123,21 +121,25 @@ namespace Orbital.Cli
                 foreach (var body in bodies)
                 {
                     // Viewport relative ratio
-                    Vector3 bmpVector = body.Position / viewportVector;
+                    Vector3 bodyViewportVector = body.Position / viewportVector;
 
                     // Scale
-                    bmpVector.X *= width / 2;
-                    bmpVector.Y *= height / 2;
+                    bodyViewportVector.X *= width / 2;
+                    bodyViewportVector.Y *= height / 2;
 
                     // Offset
-                    bmpVector.X += width / 2;
-                    bmpVector.Y += height / 2;
+                    bodyViewportVector.X += width / 2;
+                    bodyViewportVector.Y += height / 2;
 
-                    Color color = body.Metadata.Name == "Sun" ? Color.Yellow : Color.White;
+                    // Centering
+                    float ellipseDiameter = (float)(body.Radius * minEllipseRadius / minBodyRadius);
+                    float renderX = (float)bodyViewportVector.X - (ellipseDiameter / 2);
+                    float renderY = (float)bodyViewportVector.Y - (ellipseDiameter / 2);
 
-                    float radius = 4f;
+                    // Colouring (if any) TO-DO
+                    Brush brush = new SolidBrush(body.Metadata.Color);
 
-                    graphics.FillEllipse(new SolidBrush(color), (float)bmpVector.X, (float)bmpVector.Y, radius, radius);
+                    graphics.FillEllipse(brush, renderX, renderY, ellipseDiameter, ellipseDiameter);
                 }
 
                 bitmap.Save(Path.Combine(currentDirectory, directoryName, $"{i++}.png"), ImageFormat.Png);
--- Orbital/Metadata.cs
+++ Orbital/Metadata.cs
@@ -1,12 +1,16 @@
-﻿namespace Orbital
+﻿using System.Drawing;
+
+namespace Orbital
 {
     public struct Metadata
     {
         public string Name { get; set; }
+        public Color Color { get; set; }
 
-        public Metadata(string name)
+        public Metadata(string name, Color color)
         {
             Name = name;
+            Color = color;
         }
     }
 }
