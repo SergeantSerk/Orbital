b634ba8c43a074edea7de5227fedf01721d2f615
SergeantSerk <14278530+SergeantSerk@users.noreply.github.com>
2022-04-29 15:25:17 +0100

    Added radius + code improvements
    Added radius to bodies.
    Renamed variables to better reflect their purpose:
    - Viewport indicates rendered image itself
    - UpdateDt is the physical simulation steps
    Changed drawing mode to smooth for better render.
    Added comments

--- Orbital.Cli/Program.cs
+++ Orbital.Cli/Program.cs
@@ -19,67 +19,84 @@ namespace Orbital.Cli
                     metadata: new Metadata("Sun"),
                     position: Vector3.Zero,
                     velocity: Vector3.Zero,
-                    mass: 1988500e24),
+                    mass: 1988500e24,
+                    radius: 695700e3),
                 new Body(
                     metadata: new Metadata("Mercury"),
                     position: new Vector3(57.909e9, 0, 0),
                     velocity: new Vector3(0, 47.36e3, 0),
-                    mass: 0.33010e24),
+                    mass: 0.33010e24,
+                    radius: 2439.7e3),
                 new Body(
                     metadata: new Metadata("Venus"),
                     position: new Vector3(108.21e9, 0, 0),
                     velocity: new Vector3(0, 35.02e3, 0),
-                    mass: 4.8673e24),
+                    mass: 4.8673e24,
+                    radius: 6051.8e3),
                 new Body(
                     metadata: new Metadata("Earth"),
                     position: new Vector3(149.598e9, 0, 0),
                     velocity: new Vector3(0, 29.78e3, 0),
-                    mass: 5.9722e24),
+                    mass: 5.9722e24,
+                    radius: 6371.0e3),
                 new Body(
                     metadata: new Metadata("Mars"),
                     position: new Vector3(227.956e9, 0, 0),
                     velocity: new Vector3(0, 24.07e3, 0),
-                    mass: 0.64169e24),
+                    mass: 0.64169e24,
+                    radius: 3389.5e3),
                 new Body(
                     metadata: new Metadata("Jupiter"),
                     position: new Vector3(778.479e9, 0, 0),
                     velocity: new Vector3(0, 13.06e3, 0),
-                    mass: 1898.13e24),
+                    mass: 1898.13e24,
+                    radius: 69911.0e3),
                 new Body(
                     metadata: new Metadata("Saturn"),
                     position: new Vector3(1432.041e9, 0, 0),
                     velocity: new Vector3(0, 9.68e3, 0),
-                    mass: 568.32e24),
+                    mass: 568.32e24,
+                    radius: 58232.0e3),
                 new Body(
                     metadata: new Metadata("Uranus"),
                     position: new Vector3(2867.043e9, 0, 0),
                     velocity: new Vector3(0, 6.8e3, 0),
-                    mass: 86.811e24),
+                    mass: 86.811e24,
+                    radius: 25362.0e3),
                 new Body(
                     metadata: new Metadata("Neptune"),
                     position: new Vector3(4514.953e9, 0, 0),
                     velocity: new Vector3(0, 5.43e3, 0),
-                    mass: 102.409e24),
+                    mass: 102.409e24,
+                    radius: 24622.0e3),
                 new Body(
                     metadata: new Metadata("Pluto"),
                     position: new Vector3(5869.656e9, 0, 0),
                     velocity: new Vector3(0, 4.67e3, 0),
-                    mass: 0.01303e24)
+                    mass: 0.01303e24,
+                    radius: 1188.0e3)
             };
 
-            double zoom = bodies
-                .Select(_ => _.Position.X)
-                .Max() * 0.2;
+            // Maximum radius with padding
+            double zoom = 0.03;
+            double viewport = bodies
+                .Select(_ => _.Position.Magnitude)
+                .Max() * zoom;
             // Add extra x% of space
-            zoom += zoom * 0.01;
-            var zoomVector = new Vector3(zoom);
+            viewport += viewport * 0.01;
+            var viewportVector = new Vector3(viewport);
 
-            bool infinite = true;
+            double minEllipseRadius = 4f;
+            double ellipseRadius = bodies
+                .Select(_ => _.Radius)
+                .Min();
+
+            bool infinite = false;
             double t_0 = 0;
             double t = t_0;
             double t_end = 86400 * 365 * 10; // approximately a decade in seconds
-            double dt = 60*60*24; // simulation step (seconds)
-            var dtVector = new Vector3(dt);
+            double updateDt = 60*60*24; // simulation step (seconds)
+            var dtVector = new Vector3(updateDt);
 
             using var pen = new Pen(Color.White, 1f);
 
@@ -100,29 +117,27 @@ namespace Orbital.Cli
                 int height = width;
                 using var bitmap = new Bitmap(width, height);
                 using Graphics graphics = Graphics.FromImage(bitmap);
-                graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighSpeed;
+                graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
                 graphics.Clear(Color.Black);
 
                 foreach (var body in bodies)
                 {
-                    Vector3 bmpVector = body.Position / zoomVector;
+                    // Viewport relative ratio
+                    Vector3 bmpVector = body.Position / viewportVector;
+
+                    // Scale
                     bmpVector.X *= width / 2;
                     bmpVector.Y *= height / 2;
 
+                    // Offset
                     bmpVector.X += width / 2;
                     bmpVector.Y += height / 2;
 
-                    Color color;
-                    if (body.Metadata.Name == "Sun")
-                    {
-                        color = Color.Yellow;
-                    }
-                    else
-                    {
-                        color = Color.White;
-                    }
+                    Color color = body.Metadata.Name == "Sun" ? Color.Yellow : Color.White;
 
-                    graphics.FillEllipse(new SolidBrush(color), (float)bmpVector.X, (float)bmpVector.Y, 8, 8);
+                    float radius = 4f;
+
+                    graphics.FillEllipse(new SolidBrush(color), (float)bmpVector.X, (float)bmpVector.Y, radius, radius);
                 }
 
                 bitmap.Save(Path.Combine(currentDirectory, directoryName, $"{i++}.png"), ImageFormat.Png);
@@ -166,7 +181,7 @@ namespace Orbital.Cli
                     body.Position += body.Velocity * dtVector;
                 }
 
-                t += dt;
+                t += updateDt;
 
                 //Thread.Sleep(100);
                 Console.SetCursorPosition(0, 0);
--- Orbital/Body.cs
+++ Orbital/Body.cs
@@ -8,13 +8,15 @@
         public Vector3 Velocity { get; set; }
 
         public double Mass { get; set; }
+        public double Radius { get; set; }
 
-        public Body(Metadata metadata, Vector3 position, Vector3 velocity, double mass)
+        public Body(Metadata metadata, Vector3 position, Vector3 velocity, double mass, double radius)
         {
             Metadata = metadata;
             Position = position;
             Velocity = velocity;
             Mass = mass;
+            Radius = radius;
         }
     }
 }
